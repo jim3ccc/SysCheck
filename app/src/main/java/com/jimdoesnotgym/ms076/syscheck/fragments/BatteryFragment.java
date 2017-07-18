56d394cb1ff9561f5306b27a288c06729c5eb245
root <jimchiang001@gmail.com>
2017-07-18 17:00:24 +0800

    added fragments and respective layouts

---
+++ app/src/main/java/com/jimdoesnotgym/ms076/syscheck/fragments/BatteryFragment.java
@@ -0,0 +1,22 @@
+package com.jimdoesnotgym.ms076.syscheck.fragments;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jimdoesnotgym.ms076.syscheck.R;
+
+/**
+ * Created by ms076 on 2017-07-18.
+ */
+
+public class BatteryFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.battery_layout, container, false);
+    }
+}
